78fe91a8dfa0575d17be9d149d31097195f5057d
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-08-29 23:32:39 -0300

    add exceptions classes and handler

---
+++ src/main/java/com/example/demo/exception/ResourceExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<String> handleInsufficientBalanceException(InsufficientBalanceException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
